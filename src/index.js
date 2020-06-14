a11cb989f407e3a130e559887f77543fa4e6d93d
keng <ohtakikenji@gmail.com>
2020-06-14 19:39:02 +0900

    implement css style loader

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import _ from 'lodash';
-// import { NAME, NiJou } from './utilities';
-// import * as utilities from './utilities.js';
-// import { NAME as NAME_OF_KENG } from './utilities';
-import Lion from './utilities';
-import Tiger from './utilities';
-
-// console.log(NiJou(43));
-// console.log(NAME);
-// console.log(utilities.NAME);
-// console.log(NAME_OF_KENG);
-console.log(Lion.say());
-console.log(Tiger.say());
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
@@ -20,3 +9,4 @@ function component() {
 }
 
 document.body.appendChild(component());
+document.body.classList.add('haikei');
