f2a23b5471035b9bb42af8b025f40365539ebc35
keng <ohtakikenji@gmail.com>
2020-06-14 23:22:31 +0900

    implement optimize-css-assets-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const outputPath = path.resolve(__dirname, 'dist');
 
@@ -58,6 +59,7 @@ module.exports = {
           },
         },
       }),
+      new OptimizeCssAssetsPlugin({}),
     ],
   },
 };
